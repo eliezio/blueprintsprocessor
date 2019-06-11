onap/ccsdk-cds

Add Kafka client services
Change-Id: I76cf52f6df10e114539c4d65620f431e0f747644
Issue-ID: CCSDK-1349
Signed-off-by: Brinda Santh <brindasanth@in.ibm.com>

ms/blueprintsprocessor/modules/commons/message-lib/src/test/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/message/service/BlueprintMessageProducerServiceTest.kt
@@ -1,0 +1,79 @@
+/*
+ *  Copyright © 2019 IBM.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.onap.ccsdk.cds.blueprintsprocessor.message.service
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.spyk
+import kotlinx.coroutines.runBlocking
+import org.junit.runner.RunWith
+import org.onap.ccsdk.cds.blueprintsprocessor.core.BluePrintProperties
+import org.onap.ccsdk.cds.blueprintsprocessor.core.BlueprintPropertyConfiguration
+import org.onap.ccsdk.cds.blueprintsprocessor.message.BluePrintMessageLibConfiguration
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.support.SendResult
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.TestPropertySource
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.util.concurrent.SettableListenableFuture
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+
+
+@RunWith(SpringRunner::class)
+@DirtiesContext
+@ContextConfiguration(classes = [BluePrintMessageLibConfiguration::class,
+    BlueprintPropertyConfiguration::class, BluePrintProperties::class])
+@TestPropertySource(properties =
+["blueprintsprocessor.messageclient.sample.type=kafka-basic-auth",
+    "blueprintsprocessor.messageclient.sample.bootstrapServers=127:0.0.1:9092",
+    "blueprintsprocessor.messageclient.sample.topic=default-topic",
+    "blueprintsprocessor.messageclient.sample.clientId=default-client-id"
+])
+open class BlueprintMessageProducerServiceTest {
+
+    @Autowired
+    lateinit var bluePrintMessageLibPropertyService: BluePrintMessageLibPropertyService
+
+    @Test
+    fun testKafkaBasicAuthClientService() {
+        runBlocking {
+            val bluePrintMessageClientService = bluePrintMessageLibPropertyService
+                    .blueprintMessageClientService("sample") as KafkaBasicAuthMessageProducerService
+
+            val mockKafkaTemplate = mockk<KafkaTemplate<String, Any>>()
+
+            val future = SettableListenableFuture<SendResult<String, Any>>()
+            //future.setException(BluePrintException("failed sending"))
+
+            every { mockKafkaTemplate.send(any(), any()) } returns future
+
+            val spyBluePrintMessageClientService = spyk(bluePrintMessageClientService, recordPrivateCalls = true)
+
+            every { spyBluePrintMessageClientService.messageTemplate(any()) } returns mockKafkaTemplate
+
+            val response = spyBluePrintMessageClientService.sendMessage("Testing message")
+            assertNotNull(response, "failed to get command response")
+        }
+    }
+
+}
+
+
+

ms/blueprintsprocessor/modules/commons/message-lib/src/main/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/message/service/BlueprintMessageProducerService.kt
@@ -1,0 +1,34 @@
+/*
+ *  Copyright © 2019 IBM.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.onap.ccsdk.cds.blueprintsprocessor.message.service
+
+import kotlinx.coroutines.runBlocking
+
+interface BlueprintMessageProducerService {
+
+    fun sendMessage(message: Any): String = runBlocking {
+        sendMessageNB(message)
+    }
+
+    fun sendMessage(topic: String, message: Any): String = runBlocking {
+        sendMessageNB(topic, message)
+    }
+
+    suspend fun sendMessageNB(message: Any): String
+
+    suspend fun sendMessageNB(topic: String, message: Any): String
+}

ms/blueprintsprocessor/modules/commons/message-lib/src/main/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/message/service/KafkaBasicAuthMessageProducerService.kt
@@ -1,0 +1,94 @@
+/*
+ *  Copyright © 2019 IBM.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.onap.ccsdk.cds.blueprintsprocessor.message.service
+
+import org.apache.kafka.clients.producer.ProducerConfig.*
+import org.apache.kafka.common.serialization.StringSerializer
+import org.onap.ccsdk.cds.blueprintsprocessor.message.KafkaBasicAuthMessageProducerProperties
+import org.onap.ccsdk.cds.controllerblueprints.core.asJsonType
+import org.slf4j.LoggerFactory
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
+import org.springframework.kafka.support.SendResult
+import org.springframework.util.concurrent.ListenableFutureCallback
+
+
+class KafkaBasicAuthMessageProducerService(
+        private val messageProducerProperties: KafkaBasicAuthMessageProducerProperties)
+    : BlueprintMessageProducerService {
+
+    private val log = LoggerFactory.getLogger(KafkaBasicAuthMessageProducerService::class.java)!!
+
+    private var kafkaTemplate: KafkaTemplate<String, Any>? = null
+
+    override suspend fun sendMessageNB(message: Any): String {
+        checkNotNull(messageProducerProperties.topic) { "default topic is not configured" }
+        return sendMessage(messageProducerProperties.topic!!, message)
+    }
+
+    override suspend fun sendMessageNB(topic: String, message: Any): String {
+        val serializedMessage = when (message) {
+            is String -> {
+                message
+            }
+            else -> {
+                message.asJsonType().toString()
+            }
+        }
+        val future = messageTemplate().send(topic, serializedMessage)
+
+        future.addCallback(object : ListenableFutureCallback<SendResult<String, Any>> {
+            override fun onSuccess(result: SendResult<String, Any>) {
+                log.info("message sent successfully with offset=[${result.recordMetadata.offset()}]")
+            }
+
+            override fun onFailure(ex: Throwable) {
+                log.error("\"Unable to send message due to [${ex.message}]")
+            }
+        })
+        return "Success"
+    }
+
+
+    private fun producerFactory(additionalConfig: Map<String, Any>? = null): ProducerFactory<String, Any> {
+        log.info("Client Properties : $messageProducerProperties")
+        val configProps = hashMapOf<String, Any>()
+        configProps[BOOTSTRAP_SERVERS_CONFIG] = messageProducerProperties.bootstrapServers
+        configProps[KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        configProps[VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        if (messageProducerProperties.clientId != null) {
+            configProps[CLIENT_ID_CONFIG] = messageProducerProperties.clientId!!
+        }
+        // TODO("Security Implementation based on type")
+
+        // Add additional Properties
+        if (additionalConfig != null) {
+            configProps.putAll(additionalConfig)
+        }
+        return DefaultKafkaProducerFactory(configProps)
+    }
+
+    fun messageTemplate(additionalConfig: Map<String, Any>? = null): KafkaTemplate<String, Any> {
+        log.info("Prepering templates")
+        if (kafkaTemplate == null) {
+            kafkaTemplate = KafkaTemplate(producerFactory(additionalConfig))
+        }
+        return kafkaTemplate!!
+    }
+}
+

eliezio/blueprintsprocessor

src/integTest/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/message/service/BlueprintMessageProducerServiceTest.kt
@@ -1,0 +1,79 @@
+/*
+ *  Copyright © 2019 IBM.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.onap.ccsdk.cds.blueprintsprocessor.message.service
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.spyk
+import kotlinx.coroutines.runBlocking
+import org.junit.runner.RunWith
+import org.onap.ccsdk.cds.blueprintsprocessor.core.BluePrintProperties
+import org.onap.ccsdk.cds.blueprintsprocessor.core.BlueprintPropertyConfiguration
+import org.onap.ccsdk.cds.blueprintsprocessor.message.BluePrintMessageLibConfiguration
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.support.SendResult
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.TestPropertySource
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.util.concurrent.SettableListenableFuture
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+
+@RunWith(SpringRunner::class)
+@DirtiesContext
+@ContextConfiguration(classes = [BluePrintMessageLibConfiguration::class,
+    BlueprintPropertyConfiguration::class, BluePrintProperties::class])
+@TestPropertySource(properties =
+["blueprintsprocessor.messageclient.sample.type=kafka-basic-auth",
+    "blueprintsprocessor.messageclient.sample.bootstrapServers=127:0.0.1:9092",
+    "blueprintsprocessor.messageclient.sample.topic=default-topic",
+    "blueprintsprocessor.messageclient.sample.clientId=default-client-id"
+])
+open class BlueprintMessageProducerServiceTest {
+
+    @Autowired
+    lateinit var bluePrintMessageLibPropertyService: BluePrintMessageLibPropertyService
+
+    @Test
+    fun testKafkaBasicAuthClientService() {
+        runBlocking {
+            val bluePrintMessageClientService = bluePrintMessageLibPropertyService
+                    .blueprintMessageClientService("sample") as KafkaBasicAuthMessageProducerService
+
+            val mockKafkaTemplate = mockk<KafkaTemplate<String, Any>>()
+
+            val future = SettableListenableFuture<SendResult<String, Any>>()
+            //future.setException(BluePrintException("failed sending"))
+
+            every { mockKafkaTemplate.send(any(), any()) } returns future
+
+            val spyBluePrintMessageClientService = spyk(bluePrintMessageClientService, recordPrivateCalls = true)
+
+            every { spyBluePrintMessageClientService.messageTemplate(any()) } returns mockKafkaTemplate
+
+            val response = spyBluePrintMessageClientService.sendMessage("Testing message")
+            assertTrue(response, "failed to get command response")
+        }
+    }
+
+}
+
+
+

src/main/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/message/service/BlueprintMessageProducerService.kt
@@ -1,0 +1,34 @@
+/*
+ *  Copyright © 2019 IBM.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.onap.ccsdk.cds.blueprintsprocessor.message.service
+
+import kotlinx.coroutines.runBlocking
+
+interface BlueprintMessageProducerService {
+
+    fun sendMessage(message: Any): Boolean = runBlocking {
+        sendMessageNB(message)
+    }
+
+    fun sendMessage(topic: String, message: Any): Boolean = runBlocking {
+        sendMessageNB(topic, message)
+    }
+
+    suspend fun sendMessageNB(message: Any): Boolean
+
+    suspend fun sendMessageNB(topic: String, message: Any): Boolean
+}

src/main/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/message/service/KafkaBasicAuthMessageProducerService.kt
@@ -1,0 +1,94 @@
+/*
+ *  Copyright © 2019 IBM.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.onap.ccsdk.cds.blueprintsprocessor.message.service
+
+import org.apache.kafka.clients.producer.ProducerConfig.*
+import org.apache.kafka.common.serialization.StringSerializer
+import org.onap.ccsdk.cds.blueprintsprocessor.message.KafkaBasicAuthMessageProducerProperties
+import org.onap.ccsdk.cds.controllerblueprints.core.asJsonType
+import org.slf4j.LoggerFactory
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
+import org.springframework.kafka.support.SendResult
+import org.springframework.util.concurrent.ListenableFutureCallback
+
+
+class KafkaBasicAuthMessageProducerService(
+        private val messageProducerProperties: KafkaBasicAuthMessageProducerProperties)
+    : BlueprintMessageProducerService {
+
+    private val log = LoggerFactory.getLogger(KafkaBasicAuthMessageProducerService::class.java)!!
+
+    private var kafkaTemplate: KafkaTemplate<String, Any>? = null
+
+    override suspend fun sendMessageNB(message: Any): Boolean {
+        checkNotNull(messageProducerProperties.topic) { "default topic is not configured" }
+        return sendMessage(messageProducerProperties.topic!!, message)
+    }
+
+    override suspend fun sendMessageNB(topic: String, message: Any): Boolean {
+        val serializedMessage = when (message) {
+            is String -> {
+                message
+            }
+            else -> {
+                message.asJsonType().toString()
+            }
+        }
+        val future = messageTemplate().send(topic, serializedMessage)
+
+        future.addCallback(object : ListenableFutureCallback<SendResult<String, Any>> {
+            override fun onSuccess(result: SendResult<String, Any>) {
+                log.info("message sent successfully with offset=[${result.recordMetadata.offset()}]")
+            }
+
+            override fun onFailure(ex: Throwable) {
+                log.error("Unable to send message", ex)
+            }
+        })
+        return true
+    }
+
+
+    private fun producerFactory(additionalConfig: Map<String, Any>? = null): ProducerFactory<String, Any> {
+        log.info("Client Properties : $messageProducerProperties")
+        val configProps = hashMapOf<String, Any>()
+        configProps[BOOTSTRAP_SERVERS_CONFIG] = messageProducerProperties.bootstrapServers
+        configProps[KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        configProps[VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        if (messageProducerProperties.clientId != null) {
+            configProps[CLIENT_ID_CONFIG] = messageProducerProperties.clientId!!
+        }
+        // TODO("Security Implementation based on type")
+
+        // Add additional Properties
+        if (additionalConfig != null) {
+            configProps.putAll(additionalConfig)
+        }
+        return DefaultKafkaProducerFactory(configProps)
+    }
+
+    fun messageTemplate(additionalConfig: Map<String, Any>? = null): KafkaTemplate<String, Any> {
+        log.info("Prepering templates")
+        if (kafkaTemplate == null) {
+            kafkaTemplate = KafkaTemplate(producerFactory(additionalConfig))
+        }
+        return kafkaTemplate!!
+    }
+}
+
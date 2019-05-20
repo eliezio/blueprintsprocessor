468c5052c00976c02b6517b4b493ef70d6ee20ac
Brinda Santh <brindasanth@in.ibm.com>
2019-05-20 15:27:55 -0400

    Add Kafka client services
    Change-Id: I76cf52f6df10e114539c4d65620f431e0f747644
    Issue-ID: CCSDK-1349
    Signed-off-by: Brinda Santh <brindasanth@in.ibm.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -111,6 +111,8 @@ dependencies {
     implementation("org.springframework.boot", "spring-boot-starter-data-jpa")
     implementation("org.springframework.boot", "spring-boot-starter-security")
 
+    implementation("org.springframework.kafka", "spring-kafka")
+
     implementation("org.apache.httpcomponents", "httpclient")
     implementation("org.apache.sshd", "sshd-core", sshdVersion)
 
